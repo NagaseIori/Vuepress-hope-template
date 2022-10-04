9a657ff76a9224952979951d30ef48a62e63e14c
NagaseIori <31349569+NagaseIori@users.noreply.github.com>
2022-10-04 07:00:53 +0000

    Deploying to gh-pages from @ NagaseIori/Vuepress-hope-template@30b45562059e4a91217a5053feb2e8f3fc1c7d8a 🚀

---
+++ assets/index.html.c77e0844.js
@@ -0,0 +1,9 @@
+import{_ as n}from"./_plugin-vue_export-helper.cdc0426e.js";import{o as s,c as e,d as a}from"./app.a9d72b2d.js";const i={},l=a(`<h2 id="\u{1F680}-install-build" tabindex="-1"><a class="header-anchor" href="#\u{1F680}-install-build" aria-hidden="true">#</a> \u{1F680} Install &amp; Build</h2><div class="language-bash ext-sh line-numbers-mode"><pre class="language-bash"><code><span class="token comment"># install pnpm</span>
+corepack <span class="token builtin class-name">enable</span>
+corepack prepare pnpm@7.11.0 <span class="token parameter variable">--activate</span>
+
+<span class="token comment"># run</span>
+<span class="token function">pnpm</span> <span class="token function">install</span>
+<span class="token function">pnpm</span> docs:dev
+<span class="token function">pnpm</span> docs:build
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,2),c=[l];function t(d,r){return s(),e("div",null,c)}const m=n(i,[["render",t],["__file","index.html.vue"]]);export{m as default};
